1e5fd50212c564f3d01ad7d30afd9b78135c83c9
sinngo <168402184+fanatsyislol@users.noreply.github.com>
2024-08-29 22:56:16 +0900

    Delete config directory

--- config/chatbot/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'chatbot'
-
-urlpatterns = [
-        path('', views.home, name='home'),
-        path('bot_response/', views.bot_response, name='bot_response'), # 応答
-]
