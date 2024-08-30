fe5768d9b6a17f7f9a4a741c6e6fb3163af3a8b6
sinngo <168402184+fanatsyislol@users.noreply.github.com>
2024-08-30 20:31:39 +0900

    Add files via upload

---
+++ chatbot/chatbotapp/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from transformers import AutoModelForQuestionAnswering, BertJapaneseTokenizer
+import torch
+from django.views.generic import TemplateView
+
+model_name = 'KoichiYasuoka/bert-base-japanese-wikipedia-ud-head'
+model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+# Create your views here.
+def home(request):
+      """
+      http://127.0.0.1:8000/で表示されるページ
+      """
+      return render(request, 'home.html')
+
+def reply(question):
+
+      tokenizer = BertJapaneseTokenizer.from_pretrained(model_name)
+
+      context = "私の名前はBです。趣味は漫画です。年齢は12歳です。出身は鹿児島です。将来の夢は獣医師です。特技はバレーボールです。苦手な人は監督です。好きな食べ物は小籠包です。飼っている犬はセブンエイトです。"
+      inputs = tokenizer.encode_plus(question, context, add_special_tokens=True, return_tensors="pt")
+      input_ids = inputs["input_ids"].tolist()[0]
+      output = model(**inputs)
+      answer_start = torch.argmax(output.start_logits)  
+      answer_end = torch.argmax(output.end_logits) + 1 
+      answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
+      answer = answer.replace(' ', '')
+
+      return answer
+
+
+def bot_response(request):
+      """
+      HTMLフォームから受信したデータを返す処理
+      http://127.0.0.1:8000/bot_response/として表示する
+      """
+
+      input_data = request.POST.get('input_text')
+      if not input_data:
+          return HttpResponse('<h2>空のデータを受け取りました。</h2>', status=400)
+
+      bot_response = reply(input_data)
+      http_response = HttpResponse()
+      http_response.write(f"BOT: {bot_response}")
+
+      return http_response
